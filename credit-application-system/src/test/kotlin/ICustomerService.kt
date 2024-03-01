e0dc03e1c07abc1f2ff65090ce4c0e94eddda3c6
Darliana <darlianacampos@gmail.com>
2024-03-01 15:39:34 -0400

    Classes

--- credit-application-system/src/test/kotlin/ICustomerService.kt
+++
@@ -1,9 +0,0 @@
-import me.dio.credit.application.system.entity.Customer
-class ICustomerService {
-
-    fun save(customer: Customer): Customer
-
-    fun findById(Id: Long): ICustomerService
-
-    fun delete(Id: Long): Customer
-}
