03ee0cb5c0e65ed66852ea98e8c5bc8f214dbe1f
Darliana <darlianacampos@gmail.com>
2024-03-05 22:05:48 -0400

    feito

--- credit-application-system/src/main/kotlin/me/dio/creditapplicationsystem/service/ICreditService.kt
+++ credit-application-system/src/main/kotlin/me/dio/creditapplicationsystem/service/ICreditService.kt
@@ -3,7 +3,7 @@ package me.dio.creditapplicationsystem.service
 import me.dio.creditapplicationsystem.entity.Credit
 import java.util.*
 
-class ICreditService {
+interface ICreditService {
     fun save(credit: Credit): Credit
     fun findAllByCustomer(customerId: Long): List<Credit>
     fun findByCreditCode(customerId: Long, creditCode: UUID): Credit
